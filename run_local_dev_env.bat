528c141bde746b9817c94d6a326e1eae6fa7f99f
Milos Milutinovic <Milos Milutinovic>
2016-11-21 18:23:53 +0100

    renamed .bat file and added a comment into it

--- run_local_dev_env.bat
+++ run_local_dev_env.bat
@@ -1,3 +1,4 @@
+:: script that runs my local pc's dev env.
 @ECHO OFF
 start cmd.exe /K "cd C:\work\scrappyCoco\backend && C: && gulp serve"
 start cmd.exe /K "cd C:\Program Files\MongoDB\Server\3.2\bin && C: && mongo"
