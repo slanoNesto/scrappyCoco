14a19882c5f704df8e7d01ca4ab6cb7098fdd490
Milos Milutinovic <Milos Milutinovic>
2016-11-21 18:19:45 +0100

    added windows batch file which runs my local dev env

---
+++ scrappy_coco.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+start cmd.exe /K "cd C:\work\scrappyCoco\backend && C: && gulp serve"
+start cmd.exe /K "cd C:\Program Files\MongoDB\Server\3.2\bin && C: && mongo"
+start /d "C:\Users\milos\AppData\Local\atom\app-1.12.5" atom.exe
